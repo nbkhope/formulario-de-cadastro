06a82d6a923112ad1924a715714ed713f785c749
nbkhope <ryumemaru@yahoo.com>
2017-11-10 09:42:01 -0800

    Commit inicial

---
+++ App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import { View, StyleSheet } from 'react-native';
+
+import Cadastro from './components/Cadastro';
+
+class App extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Cadastro />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
+
+export default App;
---
+++ components/Cadastro.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View } from 'react-native';
+
+class Cadastro extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, backgroundColor: 'green' }}>
+
+      </View>
+    );
+  }
+}
+
+export default Cadastro;
